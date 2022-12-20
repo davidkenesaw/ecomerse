bb67b310bab9add69df47cda8b0483159afe1ecd
davidkenesaw <77091094+davidkenesaw@users.noreply.github.com>
2022-12-19 20:27:35 -0500

    initialize server

---
+++ Server/app.js
@@ -0,0 +1,25 @@
+const path = require('path')
+require('dotenv').config({
+    path: path.resolve(__dirname, '../.env')
+})
+const express = require("express");
+
+//configre express app
+const app = express();
+app.set('view engine', 'ejs');//use ejs
+app.use(express.urlencoded({ extended: true }));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))//add bootsrap css
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))//add bootsrap javascript
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))//add bootsrap jquery
+app.set('views', path.join(__dirname, '../Client/views'));//show express the views directory
+app.use(express.static(path.join(__dirname, '../Client')));//show express the Client directory
+//app.use(seshOption)//configuration for express session
+
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+})
+
+app.listen(3000, ()=> {
+    console.log("Server started on port 3000");
+})
